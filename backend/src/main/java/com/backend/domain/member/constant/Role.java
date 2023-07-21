19908ff6ef17f7791e9b449a030cf4734aa53eb9
최영태 <dudxo7721@naver.com>
2023-07-21 09:54:37 +0900

    ✨ feat : Member 엔티티 생성 | 최영태 | Member, BaseEntity, Role, SocialType

---
+++ backend/src/main/java/com/backend/domain/member/constant/Role.java
@@ -0,0 +1,14 @@
+package com.backend.domain.member.constant;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+
+    USER("ROLE_USER"), ADMIN("ROLE_ADMIN"), GUEST("ROLE_GUEST");
+
+    private final String key;
+
+}
--- backend/src/main/java/com/backend/domain/member/entity/Member.java
+++ backend/src/main/java/com/backend/domain/member/entity/Member.java
@@ -1,4 +1,23 @@
 package com.backend.domain.member.entity;
 
-public class Member {
+import com.backend.domain.common.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Member extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
 }
