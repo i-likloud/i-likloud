39af7e904334e3e4ee97b47dc066549746d845b5
최영태 <dudxo7721@naver.com>
2023-07-21 10:23:39 +0900

    ♻ refactor : Member 엔티티 수정 | [최영태] | Member, MemberRepository

--- backend/src/main/java/com/backend/domain/member/entity/Member.java
+++ backend/src/main/java/com/backend/domain/member/entity/Member.java
@@ -1,6 +1,7 @@
 package com.backend.domain.member.entity;
 
 import com.backend.domain.common.BaseEntity;
+import com.backend.domain.member.constant.SocialType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -20,4 +21,20 @@ public class Member extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
+
+    private String email;
+
+    private String password;
+
+    private String nickname;
+
+    private String profile_image;
+
+    private String phone_number;
+
+    private String wallet;
+
+    private int stamp_cnt;
+
+    private SocialType socialType;
 }
