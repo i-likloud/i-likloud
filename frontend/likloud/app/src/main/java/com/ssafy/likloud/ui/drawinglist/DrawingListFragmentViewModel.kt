8ca86d9eec4c43bce09b1ab00173e97c482c15e4
DESKTOP-DOGVPUB\SSAFY <tjsgh1512@naver.com>
2023-07-24 17:59:42 +0900

    ✨ feat : 그림 목록 UI 구성 중 | 차선호 |

---
+++ frontend/likloud/app/src/main/java/com/ssafy/likloud/ui/drawinglist/DrawingListFragmentViewModel.kt
@@ -0,0 +1,23 @@
+package com.ssafy.likloud.ui.drawinglist
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ssafy.likloud.data.api.onSuccess
+import com.ssafy.likloud.data.model.UserDto
+import com.ssafy.likloud.data.repository.BaseRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class DrawingListFragmentViewModel @Inject constructor(
+    private val baseRepository: BaseRepository
+) : ViewModel() {
+
+    fun getUserInfo(userId: Int) {
+        viewModelScope.launch {
+        }
+    }
+}
