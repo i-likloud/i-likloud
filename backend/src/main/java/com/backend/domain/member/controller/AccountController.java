86fbf604cdae15ea3422a5d475ef6d2f62fe1586
DESKTOP-1JVI7QI\SSAFY <hwchoi33@naver.com>
2023-07-25 10:49:57 +0900

    ✏ modify : swagger 관련 controller 수정  | 최혜원 | OauthLoginController, LogoutController, MemberInfoController, TokenController, KakaoTokenController, NaverTokenController

---
+++ backend/src/main/java/com/backend/domain/member/controller/AccountController.java
@@ -0,0 +1,11 @@
+package com.backend.domain.member.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//@RequestMapping("/profile")
+//@RequiredArgsConstructor
+public class AccountController {
+
+}
