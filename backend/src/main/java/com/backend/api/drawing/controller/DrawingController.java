f3fc16712bab382b12945461d7c06509b542befb
최영태 <dudxo7721@naver.com>
2023-07-26 07:54:46 +0900

    config : 스웨거 수정 | 최영태 | SwaggerConfig, DrawingController

--- backend/src/main/java/com/backend/api/drawing/controller/DrawingController.java
+++ backend/src/main/java/com/backend/api/drawing/controller/DrawingController.java
@@ -7,6 +7,7 @@ import com.backend.global.error.exception.BusinessException;
 import com.backend.global.resolver.memberInfo.MemberInfo;
 import com.backend.global.resolver.memberInfo.MemberInfoDto;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -24,9 +25,7 @@ public class DrawingController {
     private final DrawingService drawingService;
 
     @PostMapping("/upload")
-    @ApiOperation(
-            value = "그림 업로드"
-            , notes = "JWT 토큰과 함께 그림 파일, 제목, 내용을 업로드한다.")
+    @Operation(summary = "그림 업로드", description = "JWT토큰과 그림 파일, 제목, 내용을 요청으로 보내서 그림을 업로드합니다.")
     public ResponseEntity<?> uploadDrawing(@RequestPart MultipartFile file, Drawing drawingRequest, @MemberInfo MemberInfoDto memberInfoDto) {
         try {
             DrawingResponseDto drawing = drawingService.saveFileAndCreateDrawings(file, drawingRequest, memberInfoDto);
@@ -37,8 +36,7 @@ public class DrawingController {
     }
 
     @GetMapping("/")
-    @ApiOperation(
-            value = "그림 게시물 전체 조회")
+    @Operation(summary = "그림 게시물 전체 조회", description = "그림 게시물들을 전체 조회합니다.")
     public ResponseEntity<List<DrawingResponseDto>> drawingBoard(){
         List<DrawingResponseDto> drawings = drawingService.getAllDrawings();
         return ResponseEntity.ok().body(drawings);
