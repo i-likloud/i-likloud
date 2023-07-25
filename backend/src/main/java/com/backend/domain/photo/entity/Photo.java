0f888619d4bf9ac20f2663493ce82a7c710d4e1a
DESKTOP-DOGVPUB\SSAFY <hoilday5303@naver.com>
2023-07-25 09:56:39 +0900

    ✨ feat : photoEntity 구현 | 김제준| Photo.java

---
+++ backend/src/main/java/com/backend/domain/photo/entity/Photo.java
@@ -0,0 +1,39 @@
+package com.backend.domain.photo.entity;
+
+import com.backend.domain.common.BaseEntity;
+import com.backend.domain.member.entity.Member;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Photo  extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long photoId;
+
+    @ManyToOne
+    private Member memberId;
+
+    private BaseEntity createdAt;
+
+    private String photoUrl;
+
+    private int pickCnt;
+
+    @Builder
+    public Photo(Member memberId, String photoUrl, int pickCnt){
+        this.memberId = memberId;
+        this.photoUrl = photoUrl;
+        this.pickCnt = pickCnt;
+    }
+
+}
