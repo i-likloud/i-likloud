97457d2637911f3986e5b1586ffa1e4a1f27b614
theo-no <tjsgh1512@naver.com>
2023-07-28 00:26:13 +0900

    ✨ feat : create PhotoDrawingDetailFragment / ViewModel & UI setting | 차선호 | [files]

---
+++ frontend/likloud/app/src/main/java/com/ssafy/likloud/ui/photolist/PhotoDrawingDetailFragmentViewModel.kt
@@ -0,0 +1,11 @@
+package com.ssafy.likloud.ui.photolist
+
+import androidx.lifecycle.ViewModel
+import com.ssafy.likloud.data.repository.BaseRepository
+import javax.inject.Inject
+
+class PhotoDrawingDetailFragmentViewModel@Inject constructor(
+    private val baseRepository: BaseRepository
+) : ViewModel() {
+
+}
