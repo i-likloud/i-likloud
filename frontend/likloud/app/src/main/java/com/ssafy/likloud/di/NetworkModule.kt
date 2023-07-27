bb040757b0b407fbbdbb38623e297466455de448
heeung <skdi6031@naver.com>
2023-07-27 15:42:45 +0900

    🛠 fix : 카메라 모달 창 글자 크기 수정 및 jwt 헤더 통신 준비 | 김희웅 |

--- frontend/likloud/app/src/main/java/com/ssafy/likloud/di/NetworkModule.kt
+++ frontend/likloud/app/src/main/java/com/ssafy/likloud/di/NetworkModule.kt
@@ -46,7 +46,7 @@ object NetworkModule {
 //            .addInterceptor(EmptyBodyInterceptor())
 //            .addInterceptor(BearerInterceptor()) // Refresh Token
 //            .addInterceptor(ErrorResponseInterceptor()) // Error Response
-            .addNetworkInterceptor(XAccessTokenInterceptor()) // JWT 자동 헤더 전송
+//            .addNetworkInterceptor(XAccessTokenInterceptor()) // JWT 자동 헤더 전송
             .addInterceptor(AddCookiesInterceptor())  //쿠키 전송
             .addInterceptor(ReceivedCookiesInterceptor()) //쿠키 추출
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
