ed0d3c8e1b127b58aa5689bd0edb71f2e393cedd
DESKTOP-DOGVPUB\SSAFY <tjsgh1512@naver.com>
2023-07-28 17:52:38 +0900

    ✨ feat : NftFragment에서 Flipping test 완료 | 차선호 |

---
+++ frontend/likloud/app/src/main/java/com/ssafy/likloud/ui/nftlist/NftListFragmentViewModel.kt
@@ -0,0 +1,11 @@
+package com.ssafy.likloud.ui.nftlist
+
+import androidx.lifecycle.ViewModel
+import com.ssafy.likloud.data.repository.BaseRepository
+import javax.inject.Inject
+
+class NftListFragmentViewModel @Inject constructor(
+    private val baseRepository: BaseRepository
+) : ViewModel() {
+
+}
