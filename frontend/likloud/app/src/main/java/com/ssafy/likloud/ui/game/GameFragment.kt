51625c77b5d792d5ba0f8f4a33f3c2025ebf2802
theo-no <tjsgh1512@naver.com>
2023-08-02 00:10:07 +0900

    ✨ feat : GameFragment에서 Image 크기, progressbar 테스트 완료 | 차선호 |

--- frontend/likloud/app/src/main/java/com/ssafy/likloud/ui/game/GameFragment.kt
+++ frontend/likloud/app/src/main/java/com/ssafy/likloud/ui/game/GameFragment.kt
@@ -1,25 +1,37 @@
 package com.ssafy.likloud.ui.game
 
+import android.animation.ValueAnimator
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.LinearInterpolator
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.internal.ViewUtils.dpToPx
 import com.ssafy.likloud.MainActivity
 import com.ssafy.likloud.R
 import com.ssafy.likloud.base.BaseFragment
 import com.ssafy.likloud.databinding.FragmentGameBinding
 import com.ssafy.likloud.ui.nftlist.NftListFragmentViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
+private const val TAG = "차선호"
 @AndroidEntryPoint
 class GameFragment : BaseFragment<FragmentGameBinding>(
     FragmentGameBinding::bind, R.layout.fragment_game) {
 
-    private val nftListFragmentViewModel : NftListFragmentViewModel by viewModels()
+    private val gameFragmentViewModel : GameFragmentViewModel by viewModels()
     private lateinit var mainActivity: MainActivity
 
     override fun onAttach(context: Context) {
@@ -35,17 +47,73 @@ class GameFragment : BaseFragment<FragmentGameBinding>(
         return super.onCreateView(inflater, container, savedInstanceState)
     }
 
-    override fun initListener() {
-
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initObserver()
+        init()
+        initListener()
+
+    }
+
+    private fun initObserver(){
+        gameFragmentViewModel.imageWidth.observe(viewLifecycleOwner){
+            lifecycleScope.launch {
+                val layoutParams = binding.imageMember.layoutParams
+                layoutParams.width = gameFragmentViewModel.imageWidth.value!!
+                binding.imageMember.layoutParams = layoutParams
+            }
+        }
+        gameFragmentViewModel.imageHeight.observe(viewLifecycleOwner){
+            lifecycleScope.launch {
+                val layoutParams = binding.imageMember.layoutParams
+                layoutParams.height = gameFragmentViewModel.imageHeight.value!!
+                binding.imageMember.layoutParams = layoutParams
+            }
+        }
+
+        gameFragmentViewModel.remainTime.observe(viewLifecycleOwner){
+            lifecycleScope.launch {
+                val progressbarTime = binding.progressbarTime
+                Log.d(TAG, "remainTime : ${gameFragmentViewModel.remainTime.value} ")
+                progressbarTime.progress = ((gameFragmentViewModel.remainTime.value!!/60.0) * 100.0).toInt()
+                Log.d(TAG, "progress : ${progressbarTime.progress} ")
+            }
+        }
+    }
+
+    private fun init(){
+
+        view?.post{
+            // Get the initial width and height of the image_member view
+            gameFragmentViewModel.changeImageWidth(binding.imageMember.width)
+            gameFragmentViewModel.changeImageHeight(binding.imageMember.height)
+
+             CoroutineScope(Dispatchers.Main).launch {
+                while(gameFragmentViewModel.imageWidth.value!! > 12){
+                    delay(200)
+                    gameFragmentViewModel.changeImageWidth(gameFragmentViewModel.imageWidth.value!! - 4)
+                    gameFragmentViewModel.changeImageHeight(gameFragmentViewModel.imageHeight.value!! - 4)
+//                    Log.d(TAG, "width / height : ${gameFragmentViewModel.imageWidth.value} / ${gameFragmentViewModel.imageHeight.value}")
+                }
+            }
+
+            // Time PROGRESS BAR
+            gameFragmentViewModel.changeRemainTime(60)
+
+            CoroutineScope(Dispatchers.Main).launch {
+                while(gameFragmentViewModel.remainTime.value!! >= 0){
+                    delay(1000)
+                    gameFragmentViewModel.changeRemainTime(gameFragmentViewModel.remainTime.value!!-1)
+                }
+            }
+        }
 
     }
 
 
+    override fun initListener() {
 
-
+    }
 }
--- frontend/likloud/app/src/main/java/com/ssafy/likloud/ui/game/GameFragmentViewModel.kt
+++ frontend/likloud/app/src/main/java/com/ssafy/likloud/ui/game/GameFragmentViewModel.kt
@@ -1,11 +1,47 @@
 package com.ssafy.likloud.ui.game
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.ssafy.likloud.data.repository.BaseRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@HiltViewModel
 class GameFragmentViewModel @Inject constructor(
     private val baseRepository: BaseRepository
 ) : ViewModel() {
+
+    /////////////////// 이미지 크기 ///////////////////////
+    private var _imageWidth = MutableLiveData<Int>()
+    val imageWidth: LiveData<Int>
+        get() = _imageWidth
+    fun changeImageWidth(width: Int){
+        viewModelScope.launch {
+            _imageWidth.value = width
+        }
+    }
+
+    private var _imageHeight = MutableLiveData<Int>()
+    val imageHeight: LiveData<Int>
+        get() = _imageHeight
+    fun changeImageHeight(height: Int){
+        viewModelScope.launch {
+            _imageHeight.value = height
+        }
+    }
+
+    ////////////////////////// 시간 PROGRESS BAR //////////////////////////////////
+    private var _remainTime = MutableLiveData<Int>()
+    val remainTime: LiveData<Int>
+        get() = _remainTime
+    fun changeRemainTime(time: Int){
+        viewModelScope.launch {
+            _remainTime.value = time
+        }
+    }
+
 
 }
