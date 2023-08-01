c0ee576193b5fef5199341e3e71949f4e81d3b3e
DESKTOP-DOGVPUB\SSAFY <tjsgh1512@naver.com>
2023-08-01 17:56:07 +0900

    ✨ feat : Game UI 하는 중 | 차선호 |

--- frontend/likloud/app/src/main/java/com/ssafy/likloud/ui/game/GameFragment.kt
+++ frontend/likloud/app/src/main/java/com/ssafy/likloud/ui/game/GameFragment.kt
@@ -1,28 +1,26 @@
-package com.ssafy.likloud.ui.nftlist
+package com.ssafy.likloud.ui.game
 
 import android.content.Context
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
-import androidx.recyclerview.widget.GridLayoutManager
 import com.ssafy.likloud.MainActivity
 import com.ssafy.likloud.R
 import com.ssafy.likloud.base.BaseFragment
-import com.ssafy.likloud.data.model.DrawingListDto
-import com.ssafy.likloud.databinding.FragmentNftListBinding
+import com.ssafy.likloud.databinding.FragmentGameBinding
+import com.ssafy.likloud.ui.nftlist.NftListFragmentViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class NftListFragment : BaseFragment<FragmentNftListBinding>(
-    FragmentNftListBinding::bind, R.layout.fragment_nft_list) {
+class GameFragment : BaseFragment<FragmentGameBinding>(
+    FragmentGameBinding::bind, R.layout.fragment_game) {
 
     private val nftListFragmentViewModel : NftListFragmentViewModel by viewModels()
     private lateinit var mainActivity: MainActivity
-
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -45,30 +43,9 @@ class NftListFragment : BaseFragment<FragmentNftListBinding>(
         super.onViewCreated(view, savedInstanceState)
 
 
-        binding.apply {
-            val nftList = initDrawingList()
-            val nftAdapter = NftListAdapter(mainActivity, nftList)
-            recyclerviewNft.apply {
-                this.adapter = nftAdapter
-                layoutManager = GridLayoutManager(mainActivity, 3)
-            }
-
-        }
-        viewLifecycleOwner.lifecycleScope.launch{
-        }
     }
 
-    private fun initDrawingList(): ArrayList<DrawingListDto>{
-        val list = ArrayList<DrawingListDto>()
 
-        list.add(DrawingListDto())
-        list.add(DrawingListDto())
-        list.add(DrawingListDto())
-        list.add(DrawingListDto())
-        list.add(DrawingListDto())
-
-        return list
-    }
 
 
 }
--- frontend/likloud/app/src/main/java/com/ssafy/likloud/ui/game/GameFragmentViewModel.kt
+++ frontend/likloud/app/src/main/java/com/ssafy/likloud/ui/game/GameFragmentViewModel.kt
@@ -1,12 +1,10 @@
-package com.ssafy.likloud.ui.nftlist
+package com.ssafy.likloud.ui.game
 
 import androidx.lifecycle.ViewModel
 import com.ssafy.likloud.data.repository.BaseRepository
-import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
-@HiltViewModel
-class NftListFragmentViewModel @Inject constructor(
+class GameFragmentViewModel @Inject constructor(
     private val baseRepository: BaseRepository
 ) : ViewModel() {
 
