ef0f91b64c6966d66a0841cf45a52f59e799f2f4
이영준 <james_22@naver.com>
2023-08-01 14:11:09 +0900

    ✨ feat : 구름 판별 성공시 화면 이동 로직 구현 | 이영준 | AfterCloudValidFragment

--- frontend/likloud/app/src/main/java/com/ssafy/likloud/ui/aftercloudvalid/AfterCloudValidFragment.kt
+++ frontend/likloud/app/src/main/java/com/ssafy/likloud/ui/aftercloudvalid/AfterCloudValidFragment.kt
@@ -1,12 +1,17 @@
 package com.ssafy.likloud.ui.aftercloudvalid
 
 import android.content.Context
+import android.content.res.Resources
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.ssafy.likloud.MainActivity
+import com.ssafy.likloud.MainActivityViewModel
 import com.ssafy.likloud.R
 import com.ssafy.likloud.base.BaseFragment
 import com.ssafy.likloud.databinding.FragmentAfterCloudValidBinding
@@ -16,7 +21,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class AfterCloudValidFragment : BaseFragment<FragmentAfterCloudValidBinding>(FragmentAfterCloudValidBinding ::bind, R.layout.fragment_after_cloud_valid ) {
 
-    private val homeFragmentViewModel : HomeFragmentViewModel by viewModels()
+    private val mainActivityViewModel : MainActivityViewModel by activityViewModels()
     private lateinit var navController: NavController
     private lateinit var mActivity: MainActivity
 
@@ -36,12 +41,27 @@ class AfterCloudValidFragment : BaseFragment<FragmentAfterCloudValidBinding>(Fra
      * 클릭 리스너를 init합니다.
      */
     override fun initListener() {
-
     }
 
     fun initView(){
         binding.buttonUploadOnly.setText(getString(R.string.upload_only))
         binding.buttonDrawInstantly.setText(getString(R.string.draw_instantly))
+        initImageMaxWidth()
+        Glide.with(requireContext())
+            .load(mainActivityViewModel.uploadingPhotoUrl)
+            .transform(CenterCrop())
+            .into(binding.imageChosenPhoto)
+    }
+
+    private fun initImageMaxWidth() {
+        val screenWidth = Resources.getSystem().displayMetrics.widthPixels
+        val maxWidth = (screenWidth * 0.7).toInt()
+
+       binding.layoutChosenPhoto.layoutParams.apply {
+           if(width > maxWidth){
+               width =maxWidth
+           }
+       }
     }
 
 }
