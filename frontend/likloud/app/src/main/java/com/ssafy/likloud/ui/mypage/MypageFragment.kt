4a0664750335f4edf0722b5a92515fa22e13acee
heeung <skdi6031@naver.com>
2023-08-02 01:07:15 +0900

    ✨ feat : 마이페이지 appbar 배경 & 환경설정 버튼 & 프로필 수정 버튼 생성 | 김희웅 | mypageFragment

--- frontend/likloud/app/src/main/java/com/ssafy/likloud/ui/mypage/MypageFragment.kt
+++ frontend/likloud/app/src/main/java/com/ssafy/likloud/ui/mypage/MypageFragment.kt
@@ -6,10 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.ssafy.likloud.MainActivity
+import com.ssafy.likloud.MainActivityViewModel
 import com.ssafy.likloud.R
 import com.ssafy.likloud.base.BaseFragment
 import com.ssafy.likloud.databinding.FragmentExampleBinding
@@ -21,6 +23,7 @@ import kotlinx.coroutines.launch
 class MypageFragment : BaseFragment<FragmentMypageBinding>(FragmentMypageBinding::bind, R.layout.fragment_mypage) {
 
     private val mypageFragmentViewModel : MypageFragmentViewModel by viewModels()
+    private val mainActivityViewModel: MainActivityViewModel by activityViewModels()
     private lateinit var mActivity: MainActivity
 
     override fun onAttach(context: Context) {
@@ -39,18 +42,36 @@ class MypageFragment : BaseFragment<FragmentMypageBinding>(FragmentMypageBinding
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initView()
         initListener()
         initAnimation()
     }
 
+    private fun initView() {
+        mActivity.changeProfileLayoutInvisible()
+        val profileColor = mainActivityViewModel.waterDropColorList[mainActivityViewModel.memberInfo.value!!.profileColor].resourceId
+        val profileFace = mainActivityViewModel.waterDropFaceList[mainActivityViewModel.memberInfo.value!!.profileFace].resourceId
+        val profileAccessory = mainActivityViewModel.waterDropAccessoryList[mainActivityViewModel.memberInfo.value!!.profileAccessory].resourceId
+        setProfileImages(profileColor, profileFace, profileAccessory)
+        binding.textviewNickname.text = mainActivityViewModel.memberInfo.value!!.nickname
+        binding.textviewTicketCnt.text = mainActivityViewModel.memberInfo.value!!.goldCoin.toString()
+        binding.textviewStampCnt.text = mainActivityViewModel.memberInfo.value!!.silverCoin.toString()
+    }
+
     override fun initListener() {
         binding.buttonBack.setOnClickListener {
+            mActivity.changeProfileLayoutVisible()
             findNavController().popBackStack()
         }
     }
 
     private fun initAnimation() {
-        binding.layoutMyCharacter.animation = AnimationUtils.loadAnimation(mActivity, R.anim.shake_up_down)
+        binding.layoutMyCharacter.animation = AnimationUtils.loadAnimation(mActivity, R.anim.rotation)
     }
 
+    private fun setProfileImages(color: Int, face: Int, accessory: Int) {
+        binding.imageColorNow.setImageResource(color)
+        binding.imageFaceNow.setImageResource(face)
+        binding.imageAccessoryNow.setImageResource(accessory)
+    }
 }
