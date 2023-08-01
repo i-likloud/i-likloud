d5732ec403b1d1b9c7805799288d4e25bddd2cec
김제준 <hoilday5303@naver.com>
2023-08-01 12:46:17 +0900

    ✏ modify : 그림 전체 조회 수정 | 김제준 | DrawingResponseDto.java

--- backend/src/main/java/com/backend/domain/drawing/dto/DrawingResponseDto.java
+++ backend/src/main/java/com/backend/domain/drawing/dto/DrawingResponseDto.java
@@ -9,14 +9,12 @@ public class DrawingResponseDto {
     private final Long drawingId;
     private final String title;
     private final String artist;
-    private final Long memberId;
     private final String imageUrl;
 
     public DrawingResponseDto(Drawing drawing) {
         this.drawingId = drawing.getDrawingId();
         this.title = drawing.getTitle();
         this.artist = drawing.getArtist();
-        this.memberId = drawing.getMember().getMemberId();
         this.imageUrl = drawing.getImageUrl();
     }
 
