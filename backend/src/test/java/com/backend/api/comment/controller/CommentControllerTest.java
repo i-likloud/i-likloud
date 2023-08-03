86cc4fb19e003cc1dc7b89b42788d37509f0912d
김제준 <hoilday5303@naver.com>
2023-08-03 16:44:17 +0900

    ✨ feat :  댓글 테스트 코드 구현 | 김제준 | CommentControllerTest.java

---
+++ backend/src/test/java/com/backend/api/comment/controller/CommentControllerTest.java
@@ -0,0 +1,81 @@
+package com.backend.api.comment.controller;
+
+import com.backend.api.common.AccessToken;
+import com.backend.api.common.BaseIntegrationTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+class CommentControllerTest extends BaseIntegrationTest {
+
+    private AccessToken accessToken;
+    private String token = AccessToken.getToken();
+    private String userEmail = AccessToken.getTestEmail();
+
+    @Test
+    @Rollback
+    void createComment() throws Exception {
+
+        // given
+        Long drawingId = 1L;
+        String content = "댓글 내용";
+
+        try {
+            //when
+            ResultActions resultActions = mvc.perform(post("/api/comment/to/{drawingId}", drawingId)
+                            .accept(MediaType.APPLICATION_JSON)
+                            .header("Authorization", "Bearer " + token)
+                            .param("content", content)
+
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andDo(MockMvcResultHandlers.print());
+
+            //then
+            resultActions.andExpect(status().isOk());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    @Rollback
+    void createAndDeleteComment() {
+        // given
+        Long drawingId = 1L;
+        String content = "댓글 내용";
+
+        try {
+            // when
+            // 댓글 생성
+            ResultActions createResult = mvc.perform(post("/api/comment/to/{drawingId}", drawingId)
+                            .accept(MediaType.APPLICATION_JSON)
+                            .header("Authorization", "Bearer " + token)
+                            .param("content", content)
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andDo(MockMvcResultHandlers.print());
+
+            // 생성된 댓글의 ID 받아오기
+            String createResponse = createResult.andReturn().getResponse().getContentAsString();
+            Long commentId = Long.parseLong(createResponse);
+
+            // 댓글 삭제
+            ResultActions deleteResult = mvc.perform(delete("/api/comment/delete/{commentId}", commentId)
+                            .header("Authorization", "Bearer " + token))
+                    .andDo(MockMvcResultHandlers.print());
+
+            // then
+            deleteResult.andExpect(status().isOk());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
--- backend/src/test/java/com/backend/api/common/AccessToken.java
+++ backend/src/test/java/com/backend/api/common/AccessToken.java
@@ -2,7 +2,7 @@ package com.backend.api.common;
 
 public class AccessToken {
     private static final String token =
-"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJBQ0NFU1MiLCJpYXQiOjE2OTEwNDQ2NTIsImV4cCI6MTY5MTA0NjQ1MiwiZW1haWwiOiJob2lsZGF5NTMwM0BuYXZlci5jb20iLCJyb2xlIjoiTUVNQkVSIn0.r-VhIHl0TrIsqUAdSns2EOR1SfsRFovPg-JSk9Q_W4k8D8l1ZIyim3L-psNSCFVW6uUQBnx-jRZ8dHEjz_idRg";
+"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJBQ0NFU1MiLCJpYXQiOjE2OTEwNDgwNzEsImV4cCI6MTY5MTA0OTg3MSwiZW1haWwiOiJob2lsZGF5NTMwM0BuYXZlci5jb20iLCJyb2xlIjoiTUVNQkVSIn0.TvhylRcZlYBG-I4QdzjdYdgiANF5IyL5GC_lOeZaULUFJ8JUpryQ2x4QYtWGQs39aJHgvXYgb61ye-65tAVhUg";
 
     private static final String testEmail =
             "hoilday5303@naver.com";
