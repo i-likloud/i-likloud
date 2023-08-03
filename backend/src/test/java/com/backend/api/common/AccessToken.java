203ba9e24c8a465386c998bf6528b61e3a70a8ae
김제준 <hoilday5303@naver.com>
2023-08-03 15:28:22 +0900

    ✨ feat : 레포트, 마이페이지 테스트코드 구현 | 김제준 | MypageControllerTest.java, ReportControllerTest.java

--- backend/src/test/java/com/backend/api/common/AccessToken.java
+++ backend/src/test/java/com/backend/api/common/AccessToken.java
@@ -2,7 +2,8 @@ package com.backend.api.common;
 
 public class AccessToken {
     private static final String token =
-    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJBQ0NFU1MiLCJpYXQiOjE2OTEwMzEzOTUsImV4cCI6MTY5MTAzMzE5NSwiZW1haWwiOiJob2lsZGF5NTMwM0BuYXZlci5jb20iLCJyb2xlIjoiTUVNQkVSIn0.4zbZR5QkLWcbMOg5fHHD9Bk2E_aD50ckkCcE_-gDqwoSC7w7kxd3g-wwqVkrkNeVfXSLEIBNTmbdyu7VHPQLiQ";
+    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJBQ0NFU1MiLCJpYXQiOjE2OTEwNDIwMzksImV4cCI6MTY5MTA0MzgzOSwiZW1haWwiOiJob2lsZGF5NTMwM0BuYXZlci5jb20iLCJyb2xlIjoiTUVNQkVSIn0.I8KXGazARa27iNnTaWSC9Psng03vtys-MhJcs7noxtHNCB9u3c2TX3inGrUtJ2i--2JoN2SpUIQKVLzNpZ2NZg";
+
     private static final String testEmail =
             "hoilday5303@naver.com";
     public static String getToken() {
--- backend/src/test/java/com/backend/api/mypage/controller/MypageControllerTest.java
+++ backend/src/test/java/com/backend/api/mypage/controller/MypageControllerTest.java
@@ -1,40 +1,197 @@
 package com.backend.api.mypage.controller;
 
+import com.backend.api.common.AccessToken;
+import com.backend.api.common.BaseIntegrationTest;
+import com.backend.api.mypage.dto.MypageInfoDto;
+import com.backend.api.mypage.service.MypageService;
+import com.backend.domain.member.constant.Role;
+import com.backend.domain.member.entity.Member;
+import com.backend.domain.member.repository.MemberRepository;
+import com.backend.domain.member.service.MemberService;
+import com.backend.global.resolver.memberInfo.MemberInfo;
+import com.backend.global.resolver.memberInfo.MemberInfoDto;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-class MypageControllerTest {
+@SpringBootTest
+class MypageControllerTest extends BaseIntegrationTest {
+    @Autowired
+    private MockMvc mvc;
+
+    private AccessToken accessToken;
+
+    private String token = AccessToken.getToken();
+    private String userEmail = AccessToken.getTestEmail();
 
     @Test
+    @Rollback
     void getMyInfo() {
+        //given
+        Long memberId = 1L;
+        //when
+        try {
+            ResultActions resultActions = mvc.perform(get("/api/mypage/home")
+                            .accept(MediaType.APPLICATION_JSON)
+                            .header("Authorization", "Bearer " + token)) // "Authorization" 헤더에 토큰 추가
+                    .andDo(MockMvcResultHandlers.print());
+
+
+
+            //then
+            resultActions
+                    .andExpect(status().isOk());
+//                    .andExpect(jsonPath("$").value(1));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
+//    @Test
+//    @Rollback
+//    void editNickname(@MemberInfo MemberInfoDto memberInfoDto) throws Exception {
+//
+//    }
+//
+//    @Test
+//    void editProfile() {
+//    }
+//
     @Test
-    void editNickname() {
-    }
-
-    @Test
-    void editProfile() {
-    }
-
-    @Test
+    @Rollback
     void getMyLikes() {
+        //given
+        Long memberId = 1L;
+
+        //when
+        try {
+            ResultActions resultActions = mvc.perform(get("/api/mypage/likes")
+                            .accept(MediaType.APPLICATION_JSON)
+                            .header("Authorization", "Bearer " + token)) // "Authorization" 헤더에 토큰 추가
+                    .andDo(MockMvcResultHandlers.print());
+
+            // 응답 상태를 확인합니다.
+            resultActions.andExpect(status().isOk())
+                    .andExpect(jsonPath("$").isArray());
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     @Test
+    @Rollback
     void getMyDrawings() {
+        //given
+        Long memberId = 1L;
+        //when
+        try {
+            ResultActions resultActions = mvc.perform(get("/api/mypage/drawings")
+                            .accept(MediaType.APPLICATION_JSON)
+                            .header("Authorization", "Bearer " + token)) // "Authorization" 헤더에 토큰 추가
+                    .andDo(MockMvcResultHandlers.print());
+
+
+            //then
+            resultActions
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$").isArray());
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 
     @Test
+    @Rollback
     void getMyBookmarks() {
+        //given
+        Long memberId = 1L;
+        //when
+        try {
+            ResultActions resultActions = mvc.perform(get("/api/mypage/bookmarks")
+                            .accept(MediaType.APPLICATION_JSON)
+                            .header("Authorization", "Bearer " + token)) // "Authorization" 헤더에 토큰 추가
+                    .andDo(MockMvcResultHandlers.print());
+
+
+            //then
+            resultActions
+                    .andExpect(status().isOk())
+                                .andExpect(jsonPath("$").isArray());
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     @Test
+    @Rollback
     void getMyPhotos() {
+        //given
+        Long memberId = 1L;
+        //when
+        try {
+            ResultActions resultActions = mvc.perform(get("/api/mypage/photos")
+                            .accept(MediaType.APPLICATION_JSON)
+                            .header("Authorization", "Bearer " + token)) // "Authorization" 헤더에 토큰 추가
+                    .andDo(MockMvcResultHandlers.print());
+
+
+            //then
+            resultActions
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$").isArray());
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     @Test
+    @Rollback
     void getMyAccessorys() {
+        //given
+        Long memberId = 1L;
+        //when
+        try {
+            ResultActions resultActions = mvc.perform(get("/api/mypage/accessory")
+                            .accept(MediaType.APPLICATION_JSON)
+                            .header("Authorization", "Bearer " + token)) // "Authorization" 헤더에 토큰 추가
+                    .andDo(MockMvcResultHandlers.print());
+
+
+            //then
+            resultActions
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$").isArray());
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
