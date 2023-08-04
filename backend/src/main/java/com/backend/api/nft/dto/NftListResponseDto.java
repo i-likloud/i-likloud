8454214cd5f61de4e358ba300c33eceeb08565ad
최영태 <dudxo7721@naver.com>
2023-08-04 20:03:08 +0900

    🛠 fix : [NftListResponseDto Jsonproperty(캐시 에러 해결)] | [최영태] | [NftListResponseDto]

--- backend/src/main/java/com/backend/api/nft/dto/NftListResponseDto.java
+++ backend/src/main/java/com/backend/api/nft/dto/NftListResponseDto.java
@@ -1,6 +1,8 @@
 package com.backend.api.nft.dto;
 
 import com.backend.domain.nft.entity.Nft;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,6 +15,7 @@ public class NftListResponseDto {
     private String imageUrl;
     private String title;
     private String content;
+    private String tokenId;
 
     public NftListResponseDto(Nft nft) {
         this.owner = nft.getMember().getNickname();
@@ -20,5 +23,17 @@ public class NftListResponseDto {
         this.nftId = nft.getNftId();
         this.imageUrl = nft.getNftImageUrl();
         this.content = nft.getDrawing().getContent();
+        this.tokenId = nft.getTokenId();
+    }
+
+    @JsonCreator
+    public NftListResponseDto(@JsonProperty("owner") String owner, @JsonProperty("title") String title, @JsonProperty("nftId") Long nftId,
+                                @JsonProperty("imageUrl") String imageUrl, @JsonProperty("content") String content, @JsonProperty("tokenId") String tokenId) {
+        this.owner = owner;
+        this.title = title;
+        this.nftId = nftId;
+        this.imageUrl = imageUrl;
+        this.content = content;
+        this.tokenId = tokenId;
     }
 }
