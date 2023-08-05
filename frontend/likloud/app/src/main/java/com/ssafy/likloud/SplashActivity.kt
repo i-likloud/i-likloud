4486ec694d7722184f2beded99ac25490bc6c3ab
heeung <skdi6031@naver.com>
2023-08-06 01:17:46 +0900

    ✨ feat : 인트로 화면 구현 | 김희웅 | SplashActivity

---
+++ frontend/likloud/app/src/main/java/com/ssafy/likloud/SplashActivity.kt
@@ -0,0 +1,38 @@
+package com.ssafy.likloud
+
+import android.animation.ObjectAnimator
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.animation.OvershootInterpolator
+import com.ssafy.likloud.base.BaseActivity
+import com.ssafy.likloud.databinding.ActivityMainBinding
+import com.ssafy.likloud.databinding.ActivitySplashBinding
+import com.ssafy.likloud.util.makeAnimationFade
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+private const val TAG = "SplashActivity_싸피"
+@AndroidEntryPoint
+class SplashActivity : BaseActivity<ActivitySplashBinding>(ActivitySplashBinding::inflate) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(4400)
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            })
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: 사라짐!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    }
+}
