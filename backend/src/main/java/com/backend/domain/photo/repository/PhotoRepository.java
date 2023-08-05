66991bf6a292c8cc9900a1d3563c66660e57758f
최영태 <dudxo7721@naver.com>
2023-08-05 22:46:41 +0900

    Revert "🛠 fix : [사진 전체조회 쿼리 수정] | [최영태] | [PhotoRepository]"
    This reverts commit 83ba70b231c316483533e12f3d1a92e2b2009982.

--- backend/src/main/java/com/backend/domain/photo/repository/PhotoRepository.java
+++ backend/src/main/java/com/backend/domain/photo/repository/PhotoRepository.java
@@ -8,13 +8,13 @@ import java.util.List;
 
 public interface PhotoRepository extends JpaRepository<Photo,Long> {
 
-    @Query("SELECT p FROM Photo p JOIN FETCH p.member JOIN FETCH p.photoFile ORDER BY p.createdAt DESC")
+    @Query("SELECT p FROM Photo p JOIN FETCH p.member JOIN FETCH p.photoFile")
     List<Photo> findAllByOrderByCreatedAtDesc();
 
-    @Query("SELECT p FROM Photo p JOIN FETCH p.member JOIN FETCH p.photoFile ORDER BY p.pickCnt DESC")
+    @Query("SELECT p FROM Photo p JOIN FETCH p.member JOIN FETCH p.photoFile")
     List<Photo> findAllByOrderByPickCntDesc();
 
-    @Query("SELECT p FROM Photo p JOIN FETCH p.member JOIN FETCH p.photoFile ORDER BY p.bookmarkCnt DESC")
+    @Query("SELECT p FROM Photo p JOIN FETCH p.member JOIN FETCH p.photoFile")
     List<Photo> findAllByOrderByBookmarkCntDesc();
 
     List<Photo> findAllByMemberMemberId(Long memberId);
