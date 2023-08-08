8ec628148e1958e911d1cd2896fd4d89d21f8f69
최혜원 <hwchoi33@naver.com>
2023-08-08 17:49:49 +0900

    ✨ feat : LikesController 예외 테스트 코드 구현  | 최혜원 |  LikesControllerTestEx

---
+++ backend/src/test/java/com/backend/api/likes/controller/LikesControllerTestEx.java
@@ -0,0 +1,112 @@
+package com.backend.api.likes.controller;
+
+import com.backend.api.common.AccessToken;
+import com.backend.api.common.BaseIntegrationTest;
+import com.backend.api.likes.service.LikesService;
+import com.backend.domain.member.entity.Member;
+import com.backend.domain.member.service.MemberService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class LikesControllerTestEx extends BaseIntegrationTest {
+
+    @Autowired
+    private MemberService memberService;
+
+    @Mock
+    private LikesService likesService;
+
+    private String token = AccessToken.getNewToken();
+    private String userEmail = AccessToken.getTestEmail();
+
+
+    @Test
+    @Rollback
+    void toggleLikeWrongDrawingId() { //404 확인 : NOT FOUND
+        //given
+        Long drawingId = 9999999L;
+        Member member = memberService.findMemberByEmail(userEmail);
+        boolean isAlreadyLiked = likesService.isAlreadyLiked(member, drawingId);
+
+        try {
+            //when
+            ResultActions resultActions = mvc.perform(post("/api/drawings/{drawingId}/likes", drawingId)
+                            .accept(MediaType.APPLICATION_JSON)
+                            .header("Authorization", "Bearer " + token)
+                            .content("{\"email\": \"" + userEmail + "\"}")
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andDo(MockMvcResultHandlers.print());
+
+            //then
+            resultActions.andExpect(status().isOk());
+
+            //토글확인
+            boolean isNowLiked = likesService.isAlreadyLiked(member, drawingId);
+            if(isAlreadyLiked){
+                if (!isNowLiked) {
+                    System.out.println("토글 실패: 좋아요 취소가 되지 않았습니다.");
+                } else {
+                    System.out.println("토글 성공: 좋아요 취소가 정상적으로 되었습니다.");
+                }
+            } else {
+                if (isNowLiked) {
+                    System.out.println("토글 실패: 좋아요가 정상적으로 되지 않았습니다.");
+                } else {
+                    System.out.println("토글 성공: 좋아요가 취소되었습니다.");
+                }
+            }
+        } catch (Exception e) {
+            // 예외 처리
+            e.printStackTrace();
+        }
+    }
+    @Test
+    @Rollback
+    void toggleLikeWrongEmail() { //해당 회원은 존재하지 않습니다. 확인
+        String userEmail = "임의의 email";
+        //given
+        Long drawingId = 1L;
+        Member member = memberService.findMemberByEmail(userEmail);
+        boolean isAlreadyLiked = likesService.isAlreadyLiked(member, drawingId);
+
+        try {
+            //when
+            ResultActions resultActions = mvc.perform(post("/api/drawings/{drawingId}/likes", drawingId)
+                            .accept(MediaType.APPLICATION_JSON)
+                            .header("Authorization", "Bearer " + token)
+                            .content("{\"email\": \"" + userEmail + "\"}")
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andDo(MockMvcResultHandlers.print());
+
+            //then
+            resultActions.andExpect(status().isOk());
+
+            //토글확인
+            boolean isNowLiked = likesService.isAlreadyLiked(member, drawingId);
+            if(isAlreadyLiked){
+                if (!isNowLiked) {
+                    System.out.println("토글 실패: 좋아요 취소가 되지 않았습니다.");
+                } else {
+                    System.out.println("토글 성공: 좋아요 취소가 정상적으로 되었습니다.");
+                }
+            } else {
+                if (isNowLiked) {
+                    System.out.println("토글 실패: 좋아요가 정상적으로 되지 않았습니다.");
+                } else {
+                    System.out.println("토글 성공: 좋아요가 취소되었습니다.");
+                }
+            }
+        } catch (Exception e) {
+            // 예외 처리
+            e.printStackTrace();
+        }
+    }
+}
