95d1f8d00b44b08332449f2b6c51a335223e9824
최영태 <dudxo7721@naver.com>
2023-08-11 15:03:38 +0900

    🛠 fix : [시간대 변경] | [최영태] | [CommentDto]

--- backend/src/main/java/com/backend/domain/comment/dto/CommentDto.java
+++ backend/src/main/java/com/backend/domain/comment/dto/CommentDto.java
@@ -1,7 +1,6 @@
 package com.backend.domain.comment.dto;
 
 import com.backend.domain.comment.entity.Comment;
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
@@ -19,7 +18,6 @@ public class CommentDto {
     private final int profileColor;
     private final int profileAccessory;
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private final LocalDateTime createdAt;
 
     public CommentDto(Comment comment) {
